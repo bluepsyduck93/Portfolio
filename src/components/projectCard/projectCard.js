a7019ef6c9ed414b5d421c56a7c9f9f94af2c957
Christine Chong <45182020+bluepsyduck93@users.noreply.github.com>
2022-02-14 02:26:30 -0800

    Add Modals

--- src/components/projectCard/projectCard.js
+++ src/components/projectCard/projectCard.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './projectCard.scss'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+
 export default  function ProjectCard({
     name,
     image,
@@ -7,26 +10,42 @@ export default  function ProjectCard({
     technology,
     link
 }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return image ? (
-        <div className="resume-card-container">
-            <div className="resume-card">
-                <div className="resume-card-name">
-                    <h3>{name}</h3>
+        <div>
+            <div className="resume-card-container">
+                <div className="resume-card">
+                    <div className="resume-card-name">
+                        <h3>{name}</h3>
+                    </div>
+                    <div className="resume-card-image-container">
+                        <img className="resume-card-image" src={image} alt={'project card'} onClick={()=>{handleOpen()}}/>
+                    </div>
                 </div>
-                <div className="resume-card-image-container">
-                    <img className="resume-card-image" src={image} alt={'project card'}/>
+                <div>
+                    {technology}
+                </div>
+                <div>
+                    {description}
+                </div>
+                <div>
+                    {link}
                 </div>
             </div>
-            <div>
-                {technology}
-            </div>
-            <div>
-                {description}
-            </div>
-            <div>
-                {link}
-            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className='project-modal'>
+                    <img className="modal-image" src={image} alt={'modal-image'}/>
+                </Box>
+            </Modal>
         </div>
+
 
     ) : (
         <div className="resume-card-container">
