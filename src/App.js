a5b74aeb38ca49b664e03619ad791cd4b1352192
Christine Chong <45182020+bluepsyduck93@users.noreply.github.com>
2022-03-01 10:30:17 -0800

    Change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import MyRoutes from "./routes";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        <HashRouter>
             <MyRoutes />
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
